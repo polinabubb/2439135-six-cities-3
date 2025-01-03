62a36db5e183fbafaa7a7d1279075ff84dd6a3f1
bubb <polinabubb@gmail.com>
2025-01-03 18:14:41 +0500

    сценарии для оффера

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,8 +1,22 @@
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
 
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  findNew(count: number): Promise<DocumentType<OfferEntity>[]>;
+  findDiscussed(count: number): Promise<DocumentType<OfferEntity>[]>;
+  exists(documentId: string): Promise<boolean>;
+
+  getPremiumOffersByCity(city: string): Promise<DocumentType<OfferEntity>[] | null>;
+  getFavoriteOffersByAuthor(authorId: string): Promise<DocumentType<OfferEntity>[] | null>;
+  changeFavoriteByIdToTrue(offerId: string, authorId: string): Promise<DocumentType<OfferEntity> | null>;
+  changeFavoriteByIdToFalse(offerId: string, authorId: string): Promise<DocumentType<OfferEntity> | null>;
 }
