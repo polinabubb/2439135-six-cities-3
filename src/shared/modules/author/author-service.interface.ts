75aeeaa7952c281f7ec314bb5efd1ac2af4c4ff5
bubb <polinabubb@gmail.com>
2025-01-03 16:15:16 +0500

    update author

--- src/shared/modules/author/author-service.interface.ts
+++ src/shared/modules/author/author-service.interface.ts
@@ -1,9 +1,11 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { AuthorEntity } from './author.entity.js';
 import { CreateAuthorDto } from './dto/create-author.dto.js';
+import { UpdateAuthorDto } from './dto/update-author.dto.js';
 
 export interface AuthorService {
   create(dto: CreateAuthorDto, salt: string): Promise<DocumentType<AuthorEntity>>;
   findByEmail(email: string): Promise<DocumentType<AuthorEntity> | null>;
   findOrCreate(dto: CreateAuthorDto, salt: string): Promise<DocumentType<AuthorEntity>>;
+  updateById(userId: string, dto: UpdateAuthorDto): Promise<DocumentType<AuthorEntity> | null>;
 }
